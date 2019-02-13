bd203aafea3682ddd9e89c19c7a726032998ef27
Brandon J Sotiroff <bsotiroff1@gmail.com>
2019-02-13 01:07:53 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,28 @@
+
+function shout(str) {
+  return string.toUpperCase();
+}
+
+function whisper(str) {
+  return string.toLowerCase();
+}
+
+function logShout(str) {
+  console.log(str.toUpperCase());
+}
+
+function logWhisper(str) {
+  console.log(str.toLowerCase());
+}
+
+function sayHiToGrandma(str) {
+  
+  if(str === "I love you, Grandma.") return 
+  if(str[0] === str[0].toLowerCase)
+    return 'I can\'t hear you!';
+   else {
+    return "YES INDEED!";
+  }
+}
+
+function 
